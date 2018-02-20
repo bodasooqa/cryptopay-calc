f2cc49e56cc83d722452107bb137f4177d52e80e
bodasooqa <bodasooqa@gmail.com>
2018-02-20 23:18:06 +0300

    base design

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,9 @@ export default {
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap.scss";
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700');
+@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 #app {
+  font-family: 'Open Sans', sans-serif;
 }
 </style>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,17 +1,105 @@
 <template>
 <div class="container">
   <h1 class="section-title">Калькулятор и конвертер криптовалют</h1>
+  <div class="row">
+    <div class="col-lg-3 offset-lg-3 currency-field">
+      <label>Из</label>
+      <select class="form-control">
+        <option selected>BTC</option>
+      </select>
+    </div>
+    <div class="col-lg-3 currency-field">
+      <label>В</label>
+      <select class="form-control">
+        <option selected>USD</option>
+      </select>
+    </div>
+    <div class="col-lg-4 offset-lg-4">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="input-group mt-3">
+            <input class="form-control" v-model="sum">
+            <div class="input-group-append">
+              <button class="input-group-text" id="basic-addon2"><span class="fa fa-calculator"></span></button>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4" v-for="i in 9" :key="i">
+          <button @click="editSum(i)" class="btn mt-2 btn-num">{{ i }}</button>
+        </div>
+        <div class="col-lg-4">
+          <button @click="editSum(0)" class="btn mt-2 btn-num">0</button>
+        </div>
+        <div class="col-lg-4">
+          <button @click="editSum('.')" class="btn mt-2 btn-num">.</button>
+        </div>
+        <div class="col-lg-4">
+          <button @click="delSum" class="btn mt-2 btn-num">Del</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'Content'
+  name: 'Content',
+  data () {
+    return {
+      sum: 0,
+      dotCount: 0
+    }
+  },
+  methods: {
+    editSum(num) {
+      if (num === '.') {
+        if (this.dotCount === 0) {
+          this.sum = String(this.sum) + String(num);
+          this.dotCount += 1;
+        } else {
+          return null;
+        }
+      } else {
+        if (String(this.sum).length < 16) {
+          this.sum = Number(String(this.sum) + String(num));
+        } else {
+          return null;
+        }
+      }
+    },
+    delSum() {
+      this.sum = 0;
+      this.dotCount = 0;
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .section-title {
   text-align: center;
+  color: #ffffff;
+  margin: 80px 0 20px;
+}
+.currency-field {
+  label {
+    color: #ffffff;
+    text-align: center;
+    width: 100%;
+  }
+}
+.btn-num {
+  width: 100%;
+  border: none;
+  border-bottom: 3px solid rgba(0, 0, 0, 0.4);
+  box-shadow: 0px 4px 5px -1px rgba(0, 0, 0, 0.5);
+  transition: 0.1s;
+  &:active {
+    border-top: 3px solid #962a20;
+    border-bottom: none !important;
+    box-shadow: 0px 4px 5px -1px rgba(0, 0, 0, 0.5) inset;
+  }
 }
 </style>
